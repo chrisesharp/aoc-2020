3e81fb8199fe40f2abc9137a6aec2de8de69cfdb
C. E. Sharp <chrisesharp@gmail.com>
2020-12-01 10:39:27 +0000

    slim down

--- day1/acct.py
+++ day1/acct.py
@@ -2,8 +2,7 @@ def find_matched_pairs(d):
     return [(i,j) for i in d for j in d if i+j == 2020]
 
 def find_matched_triples(d):
-    end=len(d)
-    return [(d[i],d[j],d[k]) for i in range(end) for j in range(i+1, end) for k in range(j+1, end) if d[i]+d[j]+d[k] == 2020]
+    return [(d[i],d[j],d[k]) for i in range(len(d)) for j in range(i+1, len(d)) for k in range(j+1, len(d)) if d[i]+d[j]+d[k] == 2020]
 
 if __name__ == '__main__':
     data = list(map(int, open("input.txt", "r").readlines()))
